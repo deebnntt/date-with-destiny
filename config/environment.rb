4b42c5ff78b15ece5fe515b7ac7e21acc16f91cb
annabush092 <annabush092@gmail.com>
2017-10-02 09:56:09 -0400

    added foursquare auth

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+client = Foursquare2::Client.new(:client_id => 'ERRF2FFXVJG2FS3J3Q1V20VMQX1CAVCKVWIJ0YCQEBMNHOUG'
+, :client_secret => 'LZK2WSFZAQARZC2ADXV2PCDDFL3Q2GBA1WHCJJBT5MNWTGQ3'
+)
